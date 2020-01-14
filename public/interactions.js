e1fc80724de945c884b906c309e09cc4bd2c0a04
Kyle Becker <gnefkow87@gmail.com>
2020-01-14 15:10:18 -0500

    App now pulls the user's name

--- public/interactions.js
+++ public/interactions.js
@@ -2,14 +2,22 @@ console.log("Interactions.js is connected!")
 
 // DOM ELEMENTS ==========
 var slider = document.getElementsByClassName("slider");
-var submitButton = document.getElementById("submitButton")
+var submitButton = document.getElementById("submitButton");
+var nameBox = document.getElementById("nameBox");
+var picBox = document.getElementById("picBox");
 
 
+var userName;
 var score = 0;
 var userScores = [];
 var type = "Five";
 
 submitButton.onclick = createUserScoreArr;
+
+function getUserName(){
+  userName = nameBox.value;
+  console.log(userName);
+}
 
 function createUserScoreArr(){
   userScores = [];
@@ -18,8 +26,10 @@ function createUserScoreArr(){
     userScores.push(score);
   });
   console.log(userScores);
+  getUserName();
   addUserScore();
   interpretUserScore();
+  alert(`Sorry, ${userName}, we don't have a backend yet, so we can't find you a buddy :(`)
 }
 
 function addUserScore(){
@@ -32,22 +42,22 @@ function interpretUserScore(){
   console.log(`interpret thinks the score is ${score}`)
   switch(true) {
     case (score < 16):
-      alert(`You are probably not a withdrawn type (not a Four, Five, or Nine`)
+      alert(`Hey ${userName}, you are probably not a withdrawn type (not a Four, Five, or Nine`)
       break;
     case (score < 31):
-      alert(`You are probably not a type ${type}`)
+      alert(`Hey ${userName},you are probably not a type ${type}`)
       break;
     case (score < 46):
-      alert(`You probably have ${type} issues or a Type Five Parent`)
+      alert(`Hey ${userName}, you probably have ${type} issues or a Type Five Parent`)
       break;
     case (score < 61):
-      alert(`You most likely have a ${type} component.`)
+      alert(`Hey ${userName}, you most likely have a ${type} component.`)
       break;
     case (score < 75):
-      alert(`You are most likely a ${type}, but you could still be another type if you are thinking too narrowly about Type ${type}`)
+      alert(`Hey ${userName}, you are most likely a ${type}, but you could still be another type if you are thinking too narrowly about Type ${type}`)
       break;
     default:
-      alert("Something went wrong, try that again!")
+      alert(`Hey ${userName}, something went wrong, try that again!`)
   }
 }
 
